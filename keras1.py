e5a5d650d9b67737f152372cfcad324cd62f8907
Ray725 <Ray725@users.noreply.github.com>
2017-12-02 10:13:26 -0500

    finish a rough machine learning model

--- keras1.py
+++ keras1.py
@@ -5,18 +5,19 @@ import keras
 from keras.models import Sequential
 model = Sequential()
 
-from keras.layers import Dense, Conv2D
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Flatten, Dropout
 
 people = pd.read_csv('/Users/Bera/Documents/Columbia/Clubs/Hackathons/YHacks/people.csv')
 train = people.ix[0:599,0:13].values.astype('int32')
 test = people.ix[0:599,14:17].values.astype('int32')
 x_val = people.ix[599:999,0:13].values.astype('int32')
 y_val = people.ix[599:999,14:17].values.astype('int32')
-
-for i in range
-model.add(Conv2D(3, (3, 1), activation='relu', input_shape=(10, 1)))
+model.add(Conv2D(4, (1, 4), activation='relu', input_shape=(1, 10)))
 model.add(Dense(4, activation='relu'))
-
 model.compile(loss='binary_crossentropy', optimizer='rmsprop')
 
-model.fit(train, test, batch_size=32, epochs=10,validation_data=(x_val, y_val))
+for row in train:
+    model.fit(train, test, batch_size=32, epochs=10, validation_data=(x_val, y_val))
+
+
+# this is Raymond's shit
---
+++ model.py
@@ -0,0 +1,89 @@
+from __future__ import print_function
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import keras
+from keras import metrics
+from keras import regularizers
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras.optimizers import Adam, RMSprop
+from keras.callbacks import TensorBoard, EarlyStopping, ModelCheckpoint
+from keras.utils import plot_model
+from keras.models import load_model
+import pandas as pd
+
+people = pd.read_csv('./people.csv')
+x_train = people.ix[0:599,0:13].values.astype('int32')
+y_train = people.ix[0:599,14:18].values.astype('int32')
+x_val = people.ix[599:999,0:13].values.astype('int32')
+y_val = people.ix[599:999,14:18].values.astype('int32')
+
+def basic_model(x_size, y_size):
+    t_model = Sequential()
+    t_model.add(Dense(100, activation="tanh", input_shape=(x_size,)))
+    t_model.add(Dense(50, activation="relu"))
+    t_model.add(Dense(y_size))
+    print(t_model.summary())
+    t_model.compile(loss='mean_squared_error',
+        optimizer=Adam(),
+        metrics=[metrics.mae])
+    return(t_model)
+
+model = basic_model(x_train.shape[1], y_train.shape[1])
+
+keras_callbacks = [
+    # ModelCheckpoint('/tmp/keras_checkpoints/model.{epoch:02d}-{val_loss:.2f}.hdf5', monitor='val_loss', save_best_only=True, verbose=2)
+    # ModelCheckpoint('/tmp/keras_checkpoints/model.{epoch:02d}.hdf5', monitor='val_loss', save_best_only=True, verbose=0)
+    TensorBoard(log_dir='/tmp/keras_logs/model_3', histogram_freq=0, write_graph=True, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None),
+    EarlyStopping(monitor='val_mean_absolute_error', patience=20, verbose=0)
+]
+
+history = model.fit(x_train, y_train,
+    batch_size=32,
+    epochs=20,
+    shuffle=True,
+    verbose=2, # Change it to 2, if wished to observe execution
+    validation_data=(x_val, y_val),
+    callbacks=keras_callbacks)
+
+train_score = model.evaluate(x_train, y_train, verbose=0)
+valid_score = model.evaluate(x_val, y_val, verbose=0)
+
+print('Train MAE: ', round(train_score[1], 4), ', Train Loss: ', round(train_score[0], 4))
+print('Val MAE: ', round(valid_score[1], 4), ', Val Loss: ', round(valid_score[0], 4))
+
+def plot_hist(h, xsize=6, ysize=10):
+    # Prepare plotting
+    fig_size = plt.rcParams["figure.figsize"]
+    plt.rcParams["figure.figsize"] = [xsize, ysize]
+    fig, axes = plt.subplots(nrows=4, ncols=4, sharex=True)
+
+    # summarize history for MAE
+    plt.subplot(211)
+    plt.plot(h['mean_absolute_error'])
+    plt.plot(h['val_mean_absolute_error'])
+    plt.title('Training vs Validation MAE')
+    plt.ylabel('MAE')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # summarize history for loss
+    plt.subplot(212)
+    plt.plot(h['loss'])
+    plt.plot(h['val_loss'])
+    plt.title('Training vs Validation Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Validation'], loc='upper left')
+
+    # Plot it all in IPython (non-interactive)
+    plt.draw()
+    plt.show()
+
+    return
+
+plot_hist(history.history, xsize=8, ysize=12)
