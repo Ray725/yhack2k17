5c6d45919c46575bb10607719824ca5a2491c148
Nahum Getachew <nahum365@me.com>
2017-12-02 17:06:06 -0500

    more shit

--- prediction_api/prediction_api.py
+++ prediction_api/prediction_api.py
@@ -18,12 +18,9 @@ def get_prediction():
         return "NoData"
 
     data_dict = json.loads(data)
-    instance = [0 if data_dict["employment"][0] is "no" else 1, data_dict["peopleCovered"], data_dict["annualIncome"], 0 if data_dict["maritalSelection"][0] is "no" else 1, data_dict["height"], data_dict["weight"], 0 if data_dict["tobaccoSelection"][0] is "no" else 1, data_dict["highRiskCount"], data_dict["mediumRiskCount"], data_dict["lowRiskCount"], 0 if data_dict["genderAssignedAtBirthSelection"][0] is "male" else 1, data_dict["age"], data_dict["latitude"], data_dict["longitude"]]
+    instance = [0 if data_dict["employment"][0] is "no" else 1, int(data_dict["peopleCovered"]), int(data_dict["annualIncome"]), 0 if data_dict["maritalSelection"][0] is "no" else 1, int(data_dict["height"]), int(data_dict["weight"]), 0 if data_dict["tobaccoSelection"][0] is "no" else 1, int(data_dict["highRiskCount"]), int(data_dict["mediumRiskCount"]), int(data_dict["lowRiskCount"]), 0 if data_dict["genderAssignedAtBirthSelection"][0] is "male" else 1, int(data_dict["age"]), int(data_dict["latitude"]), int(data_dict["longitude"])]
 
-    print(instance)
-
-    print(predict_json("autoplan-187816", "pricesv2", {"input": instance}, "v2"))
-    return "Success\n"
+    return predict_json("autoplan-187816", "pricesv2", {"input": instance}, "v2")
 
 
 def predict_json(project, model, instances, version=None):
