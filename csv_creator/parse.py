399125f6929024565ec8d841f27c564454099be7
Nahum Getachew <nahum365@me.com>
2017-12-02 21:55:27 -0500

    Added new csv and fixed parse.py

--- csv_creator/parse.py
+++ csv_creator/parse.py
@@ -7,42 +7,62 @@ import csv
 import json
 
 print("Opening participant_detail.json..")
-participant_detail_json = json.load(open('JSON/participant_detail.json'))["response"]["docs"]
+participant_detail_json = json.load(open('JSON/detail.json'))["response"]["docs"]
 print("Opening participant.json..")
 participant_json = json.load(open('JSON/participant.json'))["response"]["docs"]
 print("Opening quotes.json..")
 quotes_json = json.load(open('JSON/quotes.json'))["response"]["docs"]
 
-participants = {}
+initial = {}
+
 print("Parsing participant.json...")
 for participant in participant_json:
-    participants[participant["id"]] = participant
+    initial[participant["id"]] = participant
 
-print("Parsing quotes.json...")
-quotes = {}
+secondary = {}
+for detail in participant_detail_json:
+    last = initial.get(detail["id"])
+    if last:
+        secondary[detail["id"]] = (last, detail)
+
+
+final = []
 for quote in quotes_json:
-    quotes[quote["id"]] = quote
+    last = secondary.get(quote["id"])
+    if last:
+        final.append((last[0], last[1], quote))
+
 
 # Generate Dictionary
 people = []
 counter = 0
 print("Assembling dictionaries...")
 
-for person in participant_detail_json:
+for total_person in final:
     if counter % 1000 == 0:
         print("Row {} of {}".format(counter, len(participant_detail_json)))
     counter += 1
-    person_dict = dict()
-    person_dict["employed"] = 0 if person.get("EMPLOYMENT_STATUS") is "Unemployed" else 1
-    person_dict["peopleCovered"] = person.get("PEOPLE_COVERED")
-    person_dict["annualIncome"] = person.get("ANNUAL_INCOME")
-    person_dict["maritalStatus"] = 0 if person.get("MARITAL_STATUS") is "S" else 1
-    person_dict["height"] = person.get("HEIGHT")
-    person_dict["weight"] = person.get("WEIGHT")
-    person_dict["tobacco"] = 0 if person.get("TOBACCO") is "No" else 1
 
-    # Sum up preconditions
-    precon = person.get("PRE_CONDITIONS")
+    person_dict = {}
+
+    # participant
+    participant = total_person[0]
+    person_dict["sex"] = 0 if participant.get("sex") is "M" else 1
+    person_dict["age"] = relativedelta.relativedelta(pytz.utc.localize(datetime.today()), parser.parse(participant["DOB"])).years
+    person_dict["latitude"] = participant.get("latitude")
+    person_dict["longitude"] = participant.get("longitude")
+
+    # participant_detail
+    participant_detail = total_person[1]
+    person_dict = dict()
+    person_dict["employed"] = 0 if participant_detail.get("EMPLOYMENT_STATUS") is "Unemployed" else 1
+    person_dict["peopleCovered"] = participant_detail.get("PEOPLE_COVERED")
+    person_dict["annualIncome"] = participant_detail.get("ANNUAL_INCOME")
+    person_dict["maritalStatus"] = 0 if participant_detail.get("MARITAL_STATUS") is "S" else 1
+    person_dict["height"] = participant_detail.get("HEIGHT")
+    person_dict["weight"] = participant_detail.get("WEIGHT")
+    person_dict["tobacco"] = 0 if participant_detail.get("TOBACCO") is "No" else 1
+    precon = participant_detail.get("PRE_CONDITIONS")
     if precon:
         person_dict["highRisk"] = precon.count("High")
         person_dict["mediumRisk"] = precon.count("Medium")
@@ -52,25 +72,8 @@ for person in participant_detail_json:
         person_dict["mediumRisk"] = 0
         person_dict["lowRisk"] = 0
 
-    # Grab participant for each person
-    participant = participants.get(person["id"])
-    if not participant:
-        participants.pop(person["id"], None)
-        participant_detail_json.remove(person)
-        continue
-
-    person_dict["sex"] = 0 if participant.get("sex") is "M" else 1
-    person_dict["age"] = relativedelta.relativedelta(pytz.utc.localize(datetime.today()), parser.parse(participant["DOB"])).years
-    person_dict["latitude"] = participant.get("latitude")
-    person_dict["longitude"] = participant.get("longitude")
-
-    # Grab quote for each person
-    quote = quotes[person["id"]]
-    if not quote:
-        participants.pop(person["id"], None)
-        participant_detail_json.remove(person)
-        continue
-
+    # quotes
+    quote = total_person[2]
     person_dict["bronze"] = quote.get("BRONZE")
     person_dict["silver"] = quote.get("SILVER")
     person_dict["gold"] = quote.get("GOLD")
