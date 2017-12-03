6ca99053007c622ef1ab3988d5b620adc2295df2
Rounak Bera <rounakbera@gmail.com>
2017-12-02 21:34:43 -0500

    Index.html, App.js

--- carmansquad/src/App.js
+++ carmansquad/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">AutoPlan</h1>
+          <h1 className="App-title">ViQuote</h1>
         </header>
         <p className="App-intro">
           Input your information for predicted insurance costs!
