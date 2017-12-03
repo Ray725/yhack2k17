3c14b40579aa52cc70d41631cf06747e4610ef72
Rounak Bera <rounakbera@gmail.com>
2017-12-02 22:24:04 -0500

    Logo

--- carmansquad/src/App.js
+++ carmansquad/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './ViQuote.png';
 import SearchButton from './components/SearchButton';
 import FormContainer from './containers/FormContainer';
 import './spectre.min.css';
